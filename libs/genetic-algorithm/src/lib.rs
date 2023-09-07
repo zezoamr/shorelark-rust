b00f0cf6b7b7fcb5341f79509f582ef8c63f58c0
ziad amr shalaby <zezoamr@yandex.com>
2023-09-07 04:55:33 +0300

    evolve / genetic algorithm + unittests done

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -45,9 +45,109 @@ where
                 
                 self.mutation_method.mutate(rng, &mut child);
 
-                // TODO convert `Chromosome` back into `Individual`
-                todo!()
+                I::create(child)
+
             })
             .collect()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{selection::{RouletteWheelSelection, RankSelection}, crossover::UniformCrossover, mutation::GaussianMutation, individual::TestIndividual};
+
+    use super::*;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    fn individual(genes: &[f32]) -> TestIndividual {
+        let chromosome = genes.iter().cloned().collect();
+
+        TestIndividual::create(chromosome)
+    }
+
+    #[test]
+    fn test_with_roulette_wheel_selection() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        let mut ga = GeneticAlgorithm::new(
+            RouletteWheelSelection::new(),
+            UniformCrossover::new(),
+            GaussianMutation::new(0.5, 0.5),
+        );
+
+        let mut population = vec![
+            individual(&[0.0, 0.0, 0.0]),
+            individual(&[1.0, 1.0, 1.0]),
+            individual(&[1.0, 2.0, 1.0]),
+            individual(&[1.0, 2.0, 4.0]),
+        ];
+        
+
+        // We're running `.evolve()` a few times, so that the
+        // differences between initial and output population are
+        // easier to spot.
+        //
+        // No particular reason for a number of 10 
+        // that'd change is the *magnitude* of difference between
+        // initial and output population.
+        for _ in 0..10 {
+            population = ga.evolve(&mut rng, &mut population);
+        }
+
+        let expected_population = vec![
+            individual(&[0.44769490, 2.0648358, 4.3058133]),
+            individual(&[1.21268670, 1.5538777, 2.8869110]),
+            individual(&[1.06176780, 2.2657390, 4.4287640]),
+            individual(&[0.95909685, 2.4618788, 4.0247330]),
+        ]; // for Roulette Wheel Selection
+
+        assert_eq!(population, expected_population);
+    }
+    
+    #[test]
+    fn test_with_rank_selection() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        let mut ga = GeneticAlgorithm::new(
+            RankSelection::new(),
+            UniformCrossover::new(),
+            GaussianMutation::new(0.5, 0.5),
+        );
+
+        let mut population = vec![
+            individual(&[0.0, 0.0, 0.0]),
+            individual(&[1.0, 1.0, 1.0]),
+            individual(&[1.0, 2.0, 1.0]),
+            individual(&[1.0, 2.0, 4.0]),
+        ];
+
+        // We're running `.evolve()` a few times, so that the
+        // differences between initial and output population are
+        // easier to spot.
+        //
+        // No particular reason for a number of 10 
+        // that'd change is the *magnitude* of difference between
+        // initial and output population.
+        for _ in 0..10 {
+            population = ga.evolve(&mut rng, &mut population);
+        }
+
+        //let expected_population = vec![
+        //    individual(&[0.44769490, 2.0648358, 4.3058133]),
+        //    individual(&[1.21268670, 1.5538777, 2.8869110]),
+        //    individual(&[1.06176780, 2.2657390, 4.4287640]),
+        //    individual(&[0.95909685, 2.4618788, 4.0247330]),
+        //]; // for Roulette Wheel Selection
+
+        let expected_population = vec![
+            individual(&[0.9437746, 2.7132483, 4.413993]),
+            individual(&[0.8460895, 2.1113086, 4.568947]),
+            individual(&[0.5653255, 2.326864, 4.568947]),
+            individual(&[0.5033445, 2.326864, 4.413993] ),
+        ]; // for rank selection
+
+        assert_eq!(population, expected_population);
     }
 }
